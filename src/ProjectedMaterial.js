42939f89f821fb815fa3938e6409f42edbea32d4
Joe Pea <trusktr@gmail.com>
2022-05-06 00:16:05 -0700

    fix: wrong check caused setting a new texture not to work if not yet loaded

--- src/ProjectedMaterial.js
+++ src/ProjectedMaterial.js
@@ -31,7 +31,7 @@ export default class ProjectedMaterial extends THREE.MeshPhysicalMaterial {
     this.uniforms.projectedTexture.value = texture
     this.uniforms.isTextureLoaded.value = Boolean(texture.image)
 
-    if (!this.uniforms.isTextureLoaded) {
+    if (!this.uniforms.isTextureLoaded.value) {
       addLoadListener(texture, () => {
         this.uniforms.isTextureLoaded.value = true
 
